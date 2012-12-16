4ae8d596fdbd3894cee9fda17a8b4f1db7cbaea7
Michael Cheng <mcheng.work@gmail.com>
2012-12-16 17:04:52 +0800

    Add CLI command.

---
+++ cli.php
@@ -0,0 +1,29 @@
+#!/usr/bin/php
+<?php
+// Usage:
+// $ export queue_connection_string='Endpoint=https://<NameSpace>.servicebus.windows.net/;SharedSecretIssuer=<Issuer>;SharedSecretValue=<SharedSecret>'
+// $ php cli.php Noob add --data '{"boo":"bar","foo":"car"}'
+// $ php cli.php Noob work
+require_once __DIR__ . '/config.php';
+
+$queue_name = $argv[1];
+$action = $argv[2];
+$options = array('queue'=>$queue_name);
+$c = new PHPQueue\Cli($options);
+
+switch ($action)
+{
+    case 'add':
+        $payload_json = $argv[4];
+        $payload = json_decode($payload_json, true);
+        $c->add($payload);
+        break;
+    case 'work':
+        $c->work();
+        break;
+    case 'get':
+        break;
+    default:
+        echo "Error: No action declared...\n";
+        break;
+}
