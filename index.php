814479c99c3464fa151742bbc70c69c6b75e4d58
Michael Cheng <mcheng.work@gmail.com>
2012-12-16 14:07:02 +0800

    Update URL pattern.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 require_once __DIR__ . '/config.php';
-PHPQueue\REST::$rest_server = new \Respect\Rest\Router('/index.php/');
+PHPQueue\REST::$rest_server = new \Respect\Rest\Router('/index.php?url=');
 PHPQueue\REST::defaultRoutes();
