2bd91c86f903defc426a8f7738e704cc1ddf9e48
Michael Cheng <mcheng.work@gmail.com>
2012-12-16 14:18:51 +0800

    Add test file.

---
+++ urltest.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/config.php';
+$r3 = new \Respect\Rest\Router('/urltest.php?url=/');
+$r3->get('/', function() {
+        return 'Hello World';
+    });
+$r3->get('/hello', function() {
+        return 'Hello from Path';
+    });
+?>
