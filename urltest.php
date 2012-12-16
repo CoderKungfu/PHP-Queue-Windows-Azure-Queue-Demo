c4802fa595b790ded9dc935e0efaa234f0ad077a
Michael Cheng <mcheng.work@gmail.com>
2012-12-16 14:21:09 +0800

    Add echo statement.

--- urltest.php
+++ urltest.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/config.php';
+echo "Starting URL test.";
 $r3 = new \Respect\Rest\Router('/urltest.php?url=/');
 $r3->get('/', function() {
         return 'Hello World';
