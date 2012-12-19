b1fefd06fe7fb88edb0f6f9bfbd9be4c4be60349
Michael Cheng <mcheng.work@gmail.com>
2012-12-20 04:27:48 +0800

    Image viewer.

---
+++ full.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Full Photo</title>
+    </head>
+    <body>
+        <h2>Uploaded Photos</h2>
+        <img src="<?=$_GET['img']?>" />
+    </body>
+</html>
--- listphotos.php
+++ listphotos.php
@@ -37,16 +37,40 @@ foreach($photos as $photo)
         <style>
             ul, li{margin:0px;padding:0px;list-style:none;}
             li{float:left;margin:7px;}
+            li a{display:block;width:200px;height:200px;}
+            li a:hover{background-color:#efefef;}
+            li img{display:block;margin:0px auto 0px auto;}
+            #thumbs{width:645px;border:1px solid #333;float:left;}
+            #fullimg{width:500px;float:left; padding-left:10px;}
+            .clear{clear:both;}
         </style>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     </head>
     <body>
         <h2>Uploaded Photos</h2>
-        <ul>
-            <?php foreach($sorted_photos as $key => $photo):?>
-            <?php if (isset($photo['500'])): ?>
-            <li><a href="<?=$photo['500'] ?>"><img src="<?=$photo['200'] ?>" border="0" alt="<?=$key?>" /></a></li>
-            <?php endif; ?>
-            <?php endforeach; ?>
-        </ul>
+        <div>
+            <div id="thumbs">
+                <ul>
+                    <?php foreach($sorted_photos as $key => $photo):?>
+                    <?php if (isset($photo['500'])): ?>
+                    <li><a href="<?=$photo['500'] ?>" data-full-img="<?=$photo['full'] ?>"><img src="<?=$photo['200'] ?>" border="0" /></a></li>
+                    <?php endif; ?>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+            <div id="fullimg">
+                <?php
+                $first = array_shift($sorted_photos);
+                ?>
+                <a href="/full.php?img=<?=$first['full']?>" target="_blank"><img src="<?=$first['500']?>" /></a>
+            </div>
+        </div>
+        <script type="text/javascript">
+            $('#thumbs a').click(function(evt){
+                evt.preventDefault();
+                $('#fullimg img').attr('src', $(this).attr('href'));
+                $('#fullimg a').attr('href', '/full.php?img=' + $(this).data('fullImg'));
+            });
+        </script>
     </body>
 </html>
