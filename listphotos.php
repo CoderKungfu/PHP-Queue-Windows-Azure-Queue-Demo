4ca1c36f9122c3d26282dcabb29323d4d65d0a0d
Michael Cheng <mcheng.work@gmail.com>
2012-12-20 03:51:02 +0800

    Fix formatting.

--- listphotos.php
+++ listphotos.php
@@ -14,17 +14,17 @@ foreach($photos as $photo)
     $url = $photo['url'];
     if (strpos($url, '_') === false)
     {
-        $code_name = substr($url, 0, strpos($url, '.'));
+        $code_name = substr($url, 0, strrpos($url, '.'));
         $sorted_photos[$code_name]['full'] = $url;
     }
     else if (strpos($url, '_200') > 0)
     {
-        $code_name = substr($url, 0, strpos($url, '_'));
+        $code_name = substr($url, 0, strrpos($url, '_'));
         $sorted_photos[$code_name]['200'] = $url;
     }
     else if (strpos($url, '_500') > 0)
     {
-        $code_name = substr($url, 0, strpos($url, '_'));
+        $code_name = substr($url, 0, strrpos($url, '_'));
         $sorted_photos[$code_name]['500'] = $url;
     }
 }
@@ -35,15 +35,17 @@ foreach($photos as $photo)
     <head>
         <title>Uploaded Photos</title>
         <style>
-            ul, li{margin:0px;padding:0px;}
+            ul, li{margin:0px;padding:0px;list-style:none;}
             li{float:left;margin:7px;}
         </style>
     </head>
     <body>
         <h2>Uploaded Photos</h2>
         <ul>
-            <?php foreach($sorted_photos as $photo):?>
-            <li><a href="<?=$photo['500'] ?>"><img src="<?=$photo['200'] ?>" border="0" /></a></li>
+            <?php foreach($sorted_photos as $key => $photo):?>
+            <?php if (isset($photo['500'])): ?>
+            <li><a href="<?=$photo['500'] ?>"><img src="<?=$photo['200'] ?>" border="0" alt="<?=$key?>" /></a></li>
+            <?php endif; ?>
             <?php endforeach; ?>
         </ul>
     </body>
